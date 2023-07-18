6e688cce75837e53df8e2b8f9fb08bda5d61d08a
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-07-18 18:27:32 +0100

    test2

--- src/components/Premium.js
+++ src/components/Premium.js
@@ -1,20 +1,21 @@
-import { Authenticator } from "@aws-amplify/ui-react";
-import '@aws-amplify/ui-react/styles.css';
-import { Button } from "./Button";
+// import { Authenticator } from "@aws-amplify/ui-react";
+// import '@aws-amplify/ui-react/styles.css';
+// import { Button } from "./Button";
 const Premium = ()=> {
     return (
-        <Authenticator>
-            {({signOut})=> (
-                <div className="h-100 w-full flex items-center p-2 justify-center bg-teal-lightest font-sans">
-                    <div className="bg-white rounded shadow p-4 w-full lg:w-3/4 lg:max-w-lg">
-                        <h1>Welcome to premium</h1>
-                        <p>This is a test of authentication</p>
-                        <p>testing if it works</p>
-                        <Button onClick={signOut}>SignOut</Button>
-                    </div>
-                </div>  
-            )}
-        </Authenticator>
+        <h1>testing</h1>
+        // <Authenticator>
+        //     {({signOut})=> (
+        //         <div className="h-100 w-full flex items-center p-2 justify-center bg-teal-lightest font-sans">
+        //             <div className="bg-white rounded shadow p-4 w-full lg:w-3/4 lg:max-w-lg">
+        //                 <h1>Welcome to premium</h1>
+        //                 <p>This is a test of authentication</p>
+        //                 <p>testing if it works</p>
+        //                 <Button onClick={signOut}>SignOut</Button>
+        //             </div>
+        //         </div>  
+        //     )}
+        // </Authenticator>
     )
 };
 export default Premium;
