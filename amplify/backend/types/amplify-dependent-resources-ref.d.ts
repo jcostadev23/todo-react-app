6174b008b25067836e9677cede1fa2fe986ddf31
Aurélio Pita <jaapaurelio@gmail.com>
2023-07-30 00:24:58 +0200

    remove auth

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -6,17 +6,6 @@ export type AmplifyDependentResourcesAttributes = {
       "GraphQLAPIKeyOutput": "string"
     }
   },
-  "auth": {
-    "todoreactapp8c468fe8": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
-    }
-  },
   "storage": {
     "jcostadev23todoapp": {
       "Arn": "string",
