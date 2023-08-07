ad3684cee52125d752d1779e88194bf99a10dcf3
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-08-07 22:52:21 +0100

    SignIn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { PremiumTodos } from './components/PremiumTodos';
 import Login from './components/Login';
 
 function App() {
-
   return (
     <Router>
       <Routes>
