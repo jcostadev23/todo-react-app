2118fe3f44cd45ed6f99903a705e142465c3f748
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-09-13 22:19:27 +0100

    Input date

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,7 @@
 import { ButtonDone, ButtonPriority, ButtonDelete } from './Button';
 import { handleTodo, deleteTodo, handlePriority } from '../helpers/editTodos';
 import './todos.css';
+import { format } from 'date-fns';
 
 export function Todos ({listTodos}){
     const list = [...listTodos]
@@ -13,7 +14,7 @@ export function Todos ({listTodos}){
                         <ButtonDone color={todo.done ? 'green' : 'currentColor'} onClick={(()=> handleTodo(todo))}/>
                         {!todo.done && <ButtonPriority color={todo.priority ? 'orange' : 'currentColor'} onClick={(()=>handlePriority(todo))}/>}
                         {todo.done && <ButtonDelete color={'currentColor'} onClick={()=> deleteTodo(todo.id)}/>}
-                        {todo.task}
+                        {todo.task} {format(new Date(todo.date), 'dd-MM-yyyy')}
                     </div> 
                 </div>  
             ))}
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,63 +1,69 @@
 import React, { useState } from 'react';
-import { createId, awsDateFormat } from '../helpers/createId';
+import { createId, awsDateFormat, } from '../helpers/createId';
 import { Button, ButtonPriority } from './Button';
 import { Input } from './Input';
 import { DataStore } from "aws-amplify";
 import { Todo } from '../models/index';
 import '../styles.css';
 
-export function TodosInput () {
+export function TodosInput() {
     const [task, setTask] = useState('');
     const [priority, setPriority] = useState(false);
-   
-    async function createTodo (event) {
+    const [date, setDate] = useState(awsDateFormat)
+
+    async function createTodo(event) {
         event.preventDefault()
-        
-        if(!task){
-            return 
+
+        if (!task) {
+            return
         }
- 
-        const todo = {   
+
+        const todo = {
             id: createId(),
-            task: task.charAt(0).toUpperCase()+ task.slice(1),
+            task: task.charAt(0).toUpperCase() + task.slice(1),
             priority: priority,
             done: false,
-            date: awsDateFormat
+            date: date,
         }
-       
+
         await DataStore.save(new Todo(todo))
         setPriority(false)
         setTask('')
-        
+
     }
 
-    const checkPriority = ()=>{
-            setPriority(!priority)
-        }
+    const checkPriority = () => {
+        setPriority(!priority)
+    }
 
-    const inputField = (event)=> {
+    const inputField = (event) => {
         setTask(event.target.value)
     }
-  
+   
     return (
         <form className='form' onSubmit={createTodo}>
             <Input
-                type="text" 
+                type="text"
                 placeholder='New Todo...'
                 value={task}
-                onChange={inputField}/>
-            <ButtonPriority color={!priority ? '#a0aec0' : 'orange'} 
-                onClick={()=> checkPriority()}/>
+                onChange={inputField} />
+            <Input
+                type="date"
+                className="input"
+                onChange={(e) => {setDate(e.target.value)}}
+                value={date}/>
+            <ButtonPriority color={!priority ? '#a0aec0' : 'orange'}
+                onClick={() => checkPriority()} />
             <Button
                 className={"button button-add"}
                 label={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="icons">
-                        <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
-                    </svg>
+                    <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                </svg>
                 }
                 type='Button'
-                onClick={createTodo}/>
+                onClick={createTodo} />
         </form>
-                 
+
     )
 }
 
