d1a42f1795028f5d8b44fba3cb8ddfe10e027396
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-09-14 21:34:32 +0100

    add classname on the input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,11 +2,11 @@ import * as React from "react";
 import './input.css'
 
 export function Input(props) {
-  const { placeholder, onChange, value, type } = props;
+  const { placeholder, onChange, value, type, className } = props;
   
   return (
       <input 
-        className='input'
+        className={`input ${className}`}
         type={type}        
         onChange={onChange}
         value={value}
