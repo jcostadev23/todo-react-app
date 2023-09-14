5a0e600bbee4247f3c477d502e65f52f7c0b9a14
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-09-14 21:33:37 +0100

    adding the data choice

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { createId, awsDateFormat, } from '../helpers/createId';
+import { createId } from '../helpers/createId';
 import { Button, ButtonPriority } from './Button';
 import { Input } from './Input';
 import { DataStore } from "aws-amplify";
@@ -9,7 +9,7 @@ import '../styles.css';
 export function TodosInput() {
     const [task, setTask] = useState('');
     const [priority, setPriority] = useState(false);
-    const [date, setDate] = useState(awsDateFormat)
+    const [date, setDate] = useState("")
 
     async function createTodo(event) {
         event.preventDefault()
@@ -23,12 +23,13 @@ export function TodosInput() {
             task: task.charAt(0).toUpperCase() + task.slice(1),
             priority: priority,
             done: false,
-            date: date,
+            date: (date ? date : null),
         }
 
         await DataStore.save(new Todo(todo))
         setPriority(false)
-        setTask('')
+        setTask("")
+        setDate("")
 
     }
 
@@ -39,29 +40,31 @@ export function TodosInput() {
     const inputField = (event) => {
         setTask(event.target.value)
     }
-   
+
     return (
         <form className='form' onSubmit={createTodo}>
             <Input
+                className="input-grow"
                 type="text"
                 placeholder='New Todo...'
                 value={task}
                 onChange={inputField} />
-            <Input
-                type="date"
-                className="input"
-                onChange={(e) => {setDate(e.target.value)}}
-                value={date}/>
-            <ButtonPriority color={!priority ? '#a0aec0' : 'orange'}
-                onClick={() => checkPriority()} />
-            <Button
-                className={"button button-add"}
-                label={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="icons">
-                    <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
-                </svg>
-                }
-                type='Button'
-                onClick={createTodo} />
+            <div className='mobile-form'>
+                <Input
+                    type="date"
+                    onChange={(e) => { setDate(e.target.value) }}
+                    value={date} />
+                <ButtonPriority color={!priority ? '#a0aec0' : 'orange'}
+                    onClick={() => checkPriority()} />
+                <Button
+                    className={"button button-add"}
+                    label={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="icons">
+                        <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z" />
+                    </svg>
+                    }
+                    type='Button'
+                    onClick={createTodo} />
+            </div>
         </form>
 
     )
