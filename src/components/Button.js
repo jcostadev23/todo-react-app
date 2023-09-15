06ba7d86b39a6ab72fdcfcc01f3d8ec76e0bc58c
jcostadev23 <carlos_jesus_costa@hotmail.com>
2023-09-15 21:22:43 +0100

    Button ShowTodosDone

--- src/components/Button.js
+++ src/components/Button.js
@@ -44,12 +44,14 @@ export function ButtonDelete (props){
       </svg>
   )
 }
+
 export function ShowTodosDone (props){
-  const {d, onClick} = props
+  const {showTodosDone, onClick} = props
   return (
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#4f5050" className="icons"
         onClick={onClick}>
-        <path strokeLinecap="round" strokeLinejoin="round" d={d} />
+        <path strokeLinecap="round" strokeLinejoin="round" d={showTodosDone ? "M3 4.5h14.25M3 9h9.75M3 13.5h5.25m5.25-.75L17.25 9m0 0L21 12.75M17.25 9v12" :
+                    "M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0l-3.75-3.75M17.25 21L21 17.25"} />
       </svg>
   )
 }
